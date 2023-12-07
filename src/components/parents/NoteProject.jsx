bb7d1a0492928b104a754245bc5670d95a84b66e
misbahul45 <misbahulmuttaqin395@gmail.com>
2023-12-07 13:36:53 +0700

    adding

--- src/components/parents/NoteProject.jsx
+++ src/components/parents/NoteProject.jsx
@@ -54,8 +54,8 @@ const NoteProject = () => {
     <div className={`w-full h-screen overflow-y-scroll no-scrollbar pt-32 pb-10 sm:px-24 px-2 ${darkMode?"bg-slate-900":"bg-slate-200"}`}>
       <input disabled={save} value={title} onChange={(e)=>setTitle(e.target.value)} placeholder="Text Title........" type="text" className={`w-full text-center pb-1 pl-2 bg-transparent border-b-2 outline-none font-roboto capitalize ${darkMode?"border-slate-700 text-slate-50":" text-slate-800 border-slate-400"}`} />
       <textarea disabled={save} onKeyDown={handleEnter} onChange={(e)=>setDescribe(e.target.value)} value={describe}  className={`w-full h-full pl-2 bg-transparent outline-none py-3 ${darkMode?"text-slate-50":" text-slate-800 "}`} placeholder="Describe........" ></textarea>
-      <div className="absolute top-24 right-16">
-        <ButtonIcon onClick={save?editNote:saveNote} Icon={save?FaEdit:IoMdSave} action={save?'Edit':'Save'} className={'relative p-2 bg-gray-400 rounded-full hover:bg-gray-300 hover:scale-110 transition-all duration-300 hover:shadow-xl shadow-black group'} iconClassName={`text-xl`} actionClassName={'absolute -left-4 -bottom-9 bg-gray-600 text-slate-300 px-5 rounded-full font-semibold scale-0 group-hover:scale-100 transition-all duration-300'} />
+      <div className="absolute lg:top-24 bottom-20 right-16">
+        <ButtonIcon onClick={save?editNote:saveNote} Icon={save?FaEdit:IoMdSave} action={save?'Edit':'Save'} className={'relative p-2 bg-gray-400 rounded-full hover:bg-gray-300 hover:scale-110 transition-all duration-300 hover:shadow-xl shadow-black group'} iconClassName={`text-xl`} actionClassName={'hidden lg:block absolute -left-4 -bottom-9 bg-gray-600 text-slate-300 px-5 rounded-full font-semibold scale-0 group-hover:scale-100 transition-all duration-300'} />
       </div>
     </div>
   )
